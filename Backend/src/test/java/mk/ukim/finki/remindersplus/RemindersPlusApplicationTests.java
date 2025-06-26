ebd667683fb42e8e4a0f67e4e4c7735a286cc90d
AndrejT03 <164937981+AndrejT03@users.noreply.github.com>
2025-06-26 13:28:24 +0200

    Fixing issue that caused Actions to fail

--- Backend/src/test/java/mk/ukim/finki/remindersplus/RemindersPlusApplicationTests.java
+++ Backend/src/test/java/mk/ukim/finki/remindersplus/RemindersPlusApplicationTests.java
@@ -1,8 +1,9 @@
 package mk.ukim.finki.remindersplus;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class RemindersPlusApplicationTests {
 
